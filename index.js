fed3b566d0c0d9a8676d0e3cb676163e97f1a2bf
guillermo-delprado <galfonso.delprado@gmail.com>
2024-02-12 03:29:01 -0600

    add config.js

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ import express from 'express';
 
 import fs from "fs";
 
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require('cors');
+
+const config = require('./config');
+
+var app = express();
+
+app.use(cors(
+  config.application.cors.server
+));
 
 const app = express();
 
